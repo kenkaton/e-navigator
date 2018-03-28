054bc5b605d21046a0eebc12c506f5474ac5bd30
kenkaton <putsken@gmail.com>
2018-03-28 16:40:27 +0900

    fix user/show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :interviews
-  devise_for :users  
   root 'users#index'
-  get 'users/show'
-
+  devise_for :users
+  resources :users
+  resources :interviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
