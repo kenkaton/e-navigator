172089662aa1e51c665214d8f1760abed1b36b48
kenkaton <putsken@gmail.com>
2018-04-01 16:09:49 +0900

    use optional: true at Interview

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323112152) do
+ActiveRecord::Schema.define(version: 20180328074754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180323112152) do
     t.integer "approval"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_interviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,8 +41,12 @@ ActiveRecord::Schema.define(version: 20180323112152) do
     t.date "birthday"
     t.integer "gender", default: 0, null: false
     t.string "school"
+    t.bigint "interview_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["interview_id"], name: "index_users_on_interview_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "interviews", "users"
+  add_foreign_key "users", "interviews"
 end
