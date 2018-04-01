e96ef659e882b5013cb640b57dad19790ab6072a
kenkaton <putsken@gmail.com>
2018-04-02 02:14:36 +0900

    make nested routes of interview

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -28,7 +28,7 @@ class InterviewsController < ApplicationController
 
     respond_to do |format|
       if @interview.save
-        format.html { redirect_to @interview, notice: 'Interview was successfully created.' }
+        format.html { redirect_to user_interviews_path(current_user), notice: 'Interview was successfully created.' }
         format.json { render :show, status: :created, location: @interview }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class InterviewsController < ApplicationController
   def update
     respond_to do |format|
       if @interview.update(interview_params)
-        format.html { redirect_to @interview, notice: 'Interview was successfully updated.' }
+        format.html { redirect_to user_interviews_path(current_user), notice: 'Interview was successfully updated.' }
         format.json { render :show, status: :ok, location: @interview }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class InterviewsController < ApplicationController
   def destroy
     @interview.destroy
     respond_to do |format|
-      format.html { redirect_to interviews_url, notice: 'Interview was successfully destroyed.' }
+      format.html { redirect_to user_interviews_url, notice: 'Interview was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
   resources :users
-  resources :interviews
+  resources :users do
+    resources :interviews
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
