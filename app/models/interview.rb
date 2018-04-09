08f9a57e8b92dc5db83c641b1bcb4f4a84b86a4d
kenkaton <putsken@gmail.com>
2018-04-09 10:28:51 +0900

    add validation to check date_cannot_be_in_the_past

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,8 +1,15 @@
 class Interview < ApplicationRecord
+  validate :date_cannot_be_in_the_past
   belongs_to :user
   enum approval: { pending: 0, approve: 1, reject: 2 }
   after_update :reject_other_interviews
 
+  def date_cannot_be_in_the_past
+    if self.date.present? && self.date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
+  
   private
     def reject_other_interviews
       if self.approval == 'approve'
