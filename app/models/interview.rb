aef717f2b1250684c7d46e660d38eb28b6103f6e
kenkaton <putsken@gmail.com>
2018-04-09 09:46:56 +0900

    add reject_other_interviews method at Interview model as after_update

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,12 @@
 class Interview < ApplicationRecord
   belongs_to :user
   enum approval: { pending: 0, approve: 1, reject: 2 }
+  after_update :reject_other_interviews
+
+  private
+    def reject_other_interviews
+      if self.approval == 'approve'
+        Interview.where('user_id = ? AND NOT (id = ?)', self.user_id, self.id).update_all(approval: 'reject')
+      end
+    end
 end
