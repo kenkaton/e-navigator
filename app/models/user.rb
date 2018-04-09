b646cfb251ecd967e5f84abb66c15523d925043c
kenkaton <putsken@gmail.com>
2018-04-09 10:02:14 +0900

    add age method at user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   enum gender: { male: 0, female: 1 }
-
   has_many :interviews
+
+  def age
+    date_format = "%Y%m%d"
+    (Date.today.strftime(date_format).to_i - self.birthday&.strftime(date_format).to_i) / 10000 if self.birthday
+  end
 end
